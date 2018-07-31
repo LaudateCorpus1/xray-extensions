782be735f10cbde99da36e4337b0e28a6e670a56
RayTwitty <raytwitty@gmail.com>
2018-07-31 21:10:09 +0300

    Удалены проекты ЧН и ЗП

--- render/3967_cop_1602/R3/patch_diff.cmd
+++
@@ -1,3 +0,0 @@
-if exist xrRender_R3.dll rem xrRender_R3.dll
-..\..\..\tools\bspatch.exe xrRender_R3_orig.dll xrRender_R3.dll xrRender_R3.dll.diff 
-pause
--- render/3967_cop_1602/R4/patch_diff.cmd
+++
@@ -1,2 +0,0 @@
-..\..\..\tools\bspatch.exe xrRender_R4_orig.dll xrRender_R4.dll xrRender_R4.dll.diff 
-pause
